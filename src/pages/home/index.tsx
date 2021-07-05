00026f5397c8917f9c4731d31f7e3f29d73ae110
guitao <1244397476@qq.com>
2021-07-05 22:21:13 +0800

    更新home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,56 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { Button, Table } from 'antd'
+
+const columns: {
+    title: string,
+    dataIndex: string,
+    key: string
+}[] = [
+        {
+            title: 'ID',
+            dataIndex: 'id',
+            key: 'id',
+        },
+        {
+            title: '标题',
+            dataIndex: 'title',
+            key: 'title',
+        },
+        {
+            title: '内容',
+            dataIndex: 'body',
+            key: 'body'
+        }
+    ]
 
 const Home = () => {
+
+    const [loading, setLoading] = useState<boolean>(false);
+    const [tableData, setTableData] = useState<any>();
+
+    function fetchList(): void {
+        setLoading(true)
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(res => {
+                setLoading(false)
+                console.log("====res====", res)
+                setTableData(res)
+            })
+    }
+
+    useEffect(() => {
+        fetchList()
+        return () => {
+
+        }
+    }, [])
+
     return (
-        <div>首页</div>
+        <div>
+            <Table loading={loading} dataSource={tableData} columns={columns} />
+        </div>
     )
 }
 
