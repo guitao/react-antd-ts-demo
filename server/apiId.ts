0792c4cc26199940dac973b465afe24c7a16bb8c
guitao <1244397476@qq.com>
2021-11-08 18:28:32 +0800

    新增请求封装

---
+++ server/apiId.ts
@@ -0,0 +1,8 @@
+//对外提供的服务地址
+export const publicIp =
+    process.env.NODE_ENV === 'development' ? 'http://www.baidu.com' : ''
+//对外提供获取图片的地址
+export const logoImgIp =
+    process.env.NODE_ENV === 'development' ? '' : ''
+// 登录路由
+export const LOGIN = ''
